f52b7da8e25eab8867c2b75b5e3f965eef80f6e3
Andy Turner <a.turner@epcc.ed.ac.uk>
2019-06-08 21:25:10 +0100

    Added IMB analysis

---
+++ python-modules/synthanalysis/imb.py
@@ -0,0 +1,68 @@
+import re
+
+def get_perf_dict(filename, nodes):
+    infile = open(filename, 'r')
+    resdict = {}
+    indata = False
+    for line in infile:
+        if indata:
+            tokens = line.split()
+            if (len(tokens) < 4) or (len(tokens) > 6):
+                break
+            else:
+                size = int(tokens[0])
+                key = (nodes, size)
+                resdict[key] = float(tokens[-1])
+        else:
+           if re.search('#bytes', line):
+               indata = True
+    infile.close()
+    
+    return resdict
+
+def analyse_perf_matrix(baseline, systems, nodelist, sizelist, perfdict, invert=False):
+    basedict = perfdict[baseline]
+    ratiodict = {}
+    print("{:12s}{:s}".format("","#nodes"))
+    header = "{:>12s}".format('#bytes')
+    for nodes in nodelist:
+        header = "{:s}{:>10d}".format(header, nodes)
+    print(header)
+    for system in systems:
+        print(system)
+        sysdict = perfdict[system]
+        tdict = {}
+        for size in sizelist:
+            outstring = "{:>12d}".format(size)
+            for nodes in nodelist:
+                key = (nodes,size)
+                baseperf = basedict.get(key, None)
+                perf = sysdict.get(key, None)
+                if invert:
+                    if perf is not None:
+                        ratio = baseperf/perf
+                        tdict[key] = ratio
+                    else:
+                        ratio = 0.0
+                        tdict[key] = None
+                else:
+                    if baseperf is not None:
+                        ratio = perf/baseperf
+                        tdict[key] = ratio
+                    else:
+                        ratio = 0.0
+                        tdict[key] = None
+                outstring = outstring + "{:>10.2f}".format(ratio)
+            print(outstring)
+        ratiodict[system] = tdict
+
+    return ratiodict
+
+def get_scaling(nodelist, size, sysdict):
+    perflist = []
+    for nodes in nodelist:
+        key = (nodes,size)
+        perflist.append(sysdict.get(key, None))
+    return perflist
+
+
