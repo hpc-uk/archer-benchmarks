a6af2bc926afea53dc73da14a403b85371e8c651
Owain Kenway <o.kenway@ucl.ac.uk>
2019-07-03 14:45:41 +0100

    Fetch DNA benchmark too.

--- apps/CASTEP/fetch.sh
+++ apps/CASTEP/fetch.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
 wget "http://www.castep.org/CASTEP/Al3x3?action=download&upname=al3x3.tgz"
+wget "http://www.castep.org/CASTEP/DNA?action=download&upname=DNA.tgz"
 mv "Al3x3?action=download&upname=al3x3.tgz" al3x3.tgz
+mv "DNA?action=download&upname=DNA.tgz" DNA.tgz
