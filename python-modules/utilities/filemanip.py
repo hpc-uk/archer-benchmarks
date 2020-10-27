4da59ea9cb4f615e8a9f24a3b79cdeb1569eb909
Andy Turner <a.turner@epcc.ed.ac.uk>
2020-10-27 09:44:37 +0000

    Updates OpenSBLI results files

--- python-modules/utilities/filemanip.py
+++ python-modules/utilities/filemanip.py
@@ -17,3 +17,19 @@ def get_filelist(wdir, stem):
         sys.exit(1)
 
     return files
+
+def get_filelist_ext(wdir, extension):
+    """
+    Get list of date files in the specified directory
+    """
+
+    files = []
+    if os.path.exists(wdir):
+        files = glob(os.path.join(wdir, '*' + extension ))
+        files.sort()
+    else:
+        print(wdir)
+        sys.stderr.write("Directory does not exist: {0}".format(wdir))
+        sys.exit(1)
+
+    return files
